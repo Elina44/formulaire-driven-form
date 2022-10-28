938a1b0814f9d01af4c33c96c1a2bd7098c4679f
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-10-28 16:06:17 +0200

    re push

---
+++ src/app/sign-up/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor(
+        public lastname: string,
+        public firstname: string,
+        public email: string,
+        public password: string,
+    ) {}
+  }
